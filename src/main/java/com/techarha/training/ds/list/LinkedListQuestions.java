e90b211422e1fc6e80d18ad9f45dbd06cfb3c183
Ankit Kamboj <connect@ankitkamboj.com>
2017-08-09 14:52:18 +0530

    feat(qs) implemented finding loop in a linked list

--- src/main/java/com/techarha/training/ds/list/LinkedListQuestions.java
+++ src/main/java/com/techarha/training/ds/list/LinkedListQuestions.java
@@ -1,4 +1,25 @@
 package com.techarha.training.ds.list;
 
 public class LinkedListQuestions {
+    /**
+     * Detect a circle in Linked List.
+     */
+    public static boolean isListCyclic(List list) {
+        if(list.getHead() == null || list.getHead().getNext() == null) return false;
+
+        Node slowNode = list.getHead();
+        Node fastNode = list.getHead().getNext();
+
+        while(fastNode != null) {
+            if(fastNode == slowNode) {
+                return true;
+            }
+            if(fastNode.getNext() != null) {
+                return false;
+            }
+            fastNode = fastNode.getNext().getNext();
+            slowNode = slowNode.getNext();
+        }
+        return false;
+    }
 }
